9fc2ce19a22f670359b67d8f41a77d79d964fb91
Micaela RoAz <micaelarodriguezazcurra05@gmail.com>
2023-10-31 19:06:43 -0300

    git commit

--- Proyecto-react/src/App.jsx
+++ Proyecto-react/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +33,11 @@ function App() {
       </p>
     </>
   )
+}
+
+const Title= () => {
+  return
+  <h1> Bienvenidos  </h1>
 }
 
 export default App

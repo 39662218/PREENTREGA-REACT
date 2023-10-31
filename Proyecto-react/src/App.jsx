ca9a23b71a4cf8887cd2b2882b415ef880f0e3f7
Micaela RoAz <micaelarodriguezazcurra05@gmail.com>
2023-10-31 19:44:53 -0300

    git commit

--- Proyecto-react/src/App.jsx
+++ Proyecto-react/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import MainLayout from './layouts/MainLayout'
 
 
 function App() {
@@ -38,6 +37,17 @@ function App() {
 const Title= () => {
   return
   <h1> Bienvenidos  </h1>
+}
+
+
+function App(){
+  return (
+    <MainLayout title="Tienda Pixies">
+     <ButttonComponent>
+      
+     </ButttonComponent>
+    </MainLayout>
+  )
 }
 
 export default App
---
+++ Proyecto-react/src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+
+import React, { Children } from "react";
+
+
+const MainLayout = ({
+    title = "Tienda Pixies",
+    Children
+   
+              }) => { 
+
+
+
+    return (
+        <div>
+            <h1>{Tienda}</h1>
+            
+        </div>
+    )
+}
+readFileSync
+
+export default MainLayout
